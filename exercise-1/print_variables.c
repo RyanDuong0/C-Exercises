88830fd9fd574460f05f34048854b87ac9c9efbd
Ryan Duong <ryan.duong@aol.co.uk>
2025-11-08 18:26:03 +0000

    Added print_variables.c for more printf() examples

---
+++ exercise-1/print_variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+  int i = 10;
+  printf("i = %d\n", i);    // "i = %d\n" is a print format specification
+                          // where %d is replaced by an int value
+  return 0;
+}
